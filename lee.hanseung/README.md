bc82357a6fc8c9fd241d6061ceec0c6472160127
Hanseung Lee <hanseunglee@Hanseungs-iMac.attlocal.net>
2021-09-15 18:26:16 -0700

    edited README

--- lee.hanseung/README.md
+++ lee.hanseung/README.md
@@ -4,7 +4,9 @@
 
 - http://hanseunglee.com
 - http://hanseunglee.com/aau/wnm617/lee.hanseung/index.php
-- http://hanseunglee.com/aau/wnm617/lee.hanseung/profile.html
+- http://hanseunglee.com/aau/wnm617/lee.hanseung/about.php
+- https://hdraws.com
+- https://hdraws.com/aau/wnm617/cline.hamilton/
 
 ## Perhaps Some Notes
 
--- lee.hanseung/parts/footer.php
+++ lee.hanseung/parts/footer.php
@@ -3,9 +3,9 @@
 	 		<div class="grid gap" style="padding: 2em;">
 	         	<div class="col-xs-12 col-md-9"><a href="index.php">Hanseung Lee</a><br><p style="padding-top: 2em; font-size: 0.8em;">©2021. All Rights Reserved.</p></div>
 
-	         	<div class="col-xs-12 col-md-1 navbar"><a href="index.php">Work</a></div>
-	         	<div class="col-xs-12 col-md-1 navbar"><a href="product_list.php">About</a></div>
-	         	<div class="col-xs-12 col-md-1 navbar"><a href="product_cart.php">Resume</a></div>
+	         	<!-- <div class="col-xs-12 col-md-1 navbar"><a href="index.php">Work</a></div> -->
+	         	<!-- <div class="col-xs-12 col-md-1 navbar"><a href="product_list.php">About</a></div> -->
+	         	<!-- <div class="col-xs-12 col-md-1 navbar"><a href="product_cart.php">Resume</a></div> -->
 	         </div>
 	</div>
  </footer>
